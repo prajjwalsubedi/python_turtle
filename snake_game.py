889434cc7b5997539afe9378e26c8acdab7b264a
Prajjwal Subedi <66186489+prajjwalsubedi@users.noreply.github.com>
2022-09-30 11:41:52 +1000

    MOVEMENTS IN SNAKE UPDATE

--- snake_game.py
+++ snake_game.py
@@ -3,12 +3,12 @@ import time
 start_points = (0, -20, -40)
 turtles = []
 screen = turtle.Screen()
+screen.bgcolor("black")
 class snake():
 
     def __init__(self):
-        tim = turtle.Turtle()
+        # tim = turtle.Turtle()
         screen.setup(height=600, width=600)
-        screen.bgcolor("black")
         screen.tracer(0)
         
     def move():     
@@ -20,29 +20,38 @@ class snake():
             turtle_i.goto(postion, 0)
             turtles.append(turtle_i)
 
-        screen.update()
-        is_game_on = True
-        while is_game_on:
-            screen.update()
-            time.sleep(0.2)
-            for num in range(len(turtles) - 1, 0, -1):
-                new_x = turtles[num - 1].xcor()
-                new_y = turtles[num - 1].ycor()
-                turtles[num].goto(new_x, new_y)
-            turtles[0].fd(20)
+        
 
-class direction():
+    def up():
+        turtles[0].seth(90)
+    
+    def down():
+        turtles[0].seth(270)
 
-    def __init__(self):
-        screen.listen()
-        screen.onkey(move_fd, "w")
-        screen.onkey(move_bk, "s")
-        screen.onkey(rotate_lt, "a")
-        screen.onkey(rotate_rt, "d")
-        screen.onkey(clear, "space")
-
+    def left():
+        turtles[0].seth(180)
+    
+    def right():
+        turtles[0].seth(0)
+        
 snake.move()
-while screen.listen():
-    direction()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+screen.update()
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    for num in range(len(turtles) - 1, 0, -1):
+        new_x = turtles[num - 1].xcor()
+        new_y = turtles[num - 1].ycor()
+        turtles[num].goto(new_x, new_y)
+    turtles[0].fd(20)
+
+
 
 screen.exitonclick()
