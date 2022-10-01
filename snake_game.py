9f7431194cf3a2e3e555a258d2e9580711aca50f
Prajjwal Subedi <prajjwalsubedi95@gmail.com>
2022-10-01 16:19:08 +1000

    working on snake game commit

--- snake_game.py
+++ snake_game.py
@@ -1,17 +1,20 @@
 import turtle
 import time
+
 start_points = (0, -20, -40)
 turtles = []
 screen = turtle.Screen()
 screen.bgcolor("black")
+
+
 class snake():
 
     def __init__(self):
         # tim = turtle.Turtle()
         screen.setup(height=600, width=600)
         screen.tracer(0)
-        
-    def move():     
+
+    def move():
         for postion in start_points:
             turtle_i = turtle.Turtle()
             turtle_i.pu()
@@ -20,20 +23,19 @@ class snake():
             turtle_i.goto(postion, 0)
             turtles.append(turtle_i)
 
-        
-
     def up():
         turtles[0].seth(90)
-    
+
     def down():
         turtles[0].seth(270)
 
     def left():
         turtles[0].seth(180)
-    
+
     def right():
         turtles[0].seth(0)
-        
+
+
 snake.move()
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -51,7 +53,5 @@ while is_game_on:
         new_y = turtles[num - 1].ycor()
         turtles[num].goto(new_x, new_y)
     turtles[0].fd(20)
-
-
 
 screen.exitonclick()
